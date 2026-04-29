a0584476484e37697e643c45aa1bc3373264dd4b
Aruna322 <aruna25566@gmail.com>
2026-04-29 15:49:08 +0530

    Update dellups.py

--- dellups.py
+++ dellups.py
@@ -654,7 +654,7 @@ def main():
 
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,
+            headless=True,
             slow_mo=100
         )
 
